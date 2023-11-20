fd9dff7fc0fae61b2e51cb8f560a42623d699ba0
HIsahihi <jiuqingshui47@gmail.com>
2023-11-20 18:05:39 +0900

    訂正

---
+++ src/Review01.java
@@ -0,0 +1,26 @@
+
+public class Review01 {
+
+	public static void main(String[] args) {
+
+
+		 //税込み価格　＝　商品価格　+　税率
+		int syouhinnkakaku = 100;
+		int zeiritu = tax(syouhinnkakaku,10);
+		int zeikomikakaku = syouhinnkakaku + zeiritu;
+
+	        System.out.println(syouhinnkakaku+"円の商品の税込み価格は" +zeikomikakaku +"円（消費税は" +zeiritu+"円）です。");
+
+	}
+	public static int tax(int syouhinn,int zeiritu){
+		int zei = syouhinn * zeiritu/100;
+		return zei;
+	}
+
+
+
+
+
+	}
+
+
